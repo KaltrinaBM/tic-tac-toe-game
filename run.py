1e4df707c3ec170d33d33aa3f9c0d425c4bc892a
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2022-12-31 00:45:44 +0000

    Initial commit and add instructions and request name of the player

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def game_inst():
+
+    print("* The game is played on a grid that's "
+          "3 squares by 3 squares.")
+    print("* You choose your symbol being either 'X' or 'O'. "
+          "Opponent will get the ones that's left.")
+    print("* The first player to get 3 of her marks in a row "
+          "(up, down, across, or diagonally) is the winner.")
+    print("* When all 9 squares are full, the game is over. If no "
+          "player has 3 marks in a row, the game ends in a tie.\n")
+
+
+game_inst()
+
+
+def player_name():
+
+    while True:
+        global player_name
+        player_name = input("Enter your name please: \n")
+
+        if player_name.isalpha():
+            print(f"Nice to meet you {player_name.capitalize()}.")
+            break
+        else:
+            print("Only letters are accepted")
+
+
+player_name()
